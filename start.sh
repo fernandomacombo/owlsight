1a637f9beeb9424a7988a3ceef2eae5bbae46027
Fernando Macombo <92873560+fernandomacombo@users.noreply.github.com>
2026-02-20 18:17:15 +0200

    feito

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+echo "== Running migrate =="
+python manage.py migrate --noinput
+
+echo "== Collectstatic =="
+python manage.py collectstatic --noinput
+
+echo "== Starting gunicorn =="
+gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8080}
