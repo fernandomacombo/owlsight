794ffd5280bc51d71b3814b065d9918b149d6cdf
Fernando Macombo <92873560+fernandomacombo@users.noreply.github.com>
2026-02-22 15:41:35 +0200

    atualizado

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your views here.
+def staff_required(view_func):
+    return login_required(user_passes_test(lambda u: u.is_staff or u.is_superuser)(view_func))
+
+def superuser_required(view_func):
+    return login_required(user_passes_test(lambda u: u.is_superuser)(view_func))
+
+@staff_required
+def home(request):
+    return render(request, "dashboard/home.html")
+
+@staff_required
+def books(request):
+    return render(request, "dashboard/books.html")
+
+@superuser_required
+def users(request):
+    return render(request, "dashboard/users.html")
+
+@staff_required
+def settings_view(request):
+    return render(request, "dashboard/settings.html")
