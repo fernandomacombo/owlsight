ca83455a747c370669419b3ed4a47779852cad86
Fernando Macombo <92873560+fernandomacombo@users.noreply.github.com>
2026-02-22 21:01:39 +0200

    atualizar

--- frontend/api_urls (2).py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import api_auth
-
-urlpatterns = [
-    path("csrf/", api_auth.csrf),
-    path("auth/me/", api_auth.me),
-    path("auth/login/", api_auth.login_view),
-    path("auth/logout/", api_auth.logout_view),
-]
--- frontend/api_urls.py
+++ frontend/api_urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import api_views
+from . import api_auth
+
 
 urlpatterns = [
     path("csrf/", api_views.csrf, name="api-csrf"),
@@ -7,4 +9,9 @@ urlpatterns = [
     path("auth/me/", api_views.me, name="api-me"),
     path("auth/login/", api_views.login_view, name="api-login"),
     path("auth/logout/", api_views.logout_view, name="api-logout"),
+
+    path("csrf/", api_auth.csrf),
+    path("auth/me/", api_auth.me),
+    path("auth/login/", api_auth.login_view),
+    path("auth/logout/", api_auth.logout_view),
 ]
